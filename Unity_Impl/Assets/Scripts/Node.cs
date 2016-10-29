b50ea1afba7fb837d287d7aaf3fdee078b3dea0d
Antonin Bernardin <antonin.bernardin@gmail.com>
2016-10-30 00:54:02 +0200

    add some custom fields in Unity inspector about tree generation

---
+++ Unity_Impl/Assets/Scripts/Node.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Node<T> // generic
+{
+    public Node<T> parent = null;
+    public List<Node<T>> childs;
+    public T value;
+
+    public bool isLeaf() {
+        return (childs.Count == 0);
+    }
+    public bool isRoot() {
+        return (parent == null);
+    }
+}
---
+++ Unity_Impl/Assets/Scripts/TreeModel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "tree_", menuName = "TreeModel")]
+public class TreeModel : ScriptableObject
+{
+    public Node<Vector3> root;
+    // texture, etc...
+}
