c0e86dc8faf1e78ce7d3f0cf5eb79a52f1838431
Antonin Bernardin <antonin.bernardin@gmail.com>
2016-11-11 15:42:00 +0100

    add new attributes in Bud class

--- Unity_Impl/Assets/Scripts/Bud.cs
+++ Unity_Impl/Assets/Scripts/Bud.cs
@@ -1,16 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+public enum BudState {
+    NEW_METAMER,
+    FLOWER,
+    DORMANT,
+    ABORT
+}
+
 public class Bud
 {
     public Vector3 pos;
     public Vector3 dir;
     public List<Vector3> targetMarkers;
     public Vector3 optimalGrowth;
+    public bool isNewAxis;
+    public BudState state;
 
-    public Bud(Vector3 position) {
+    public Bud(Vector3 position, bool isNewAxis_ = false) {
+        isNewAxis = isNewAxis_;
         this.pos = position;
         targetMarkers = new List<Vector3>();
         dir = new Vector3(0, 1, 0);
+        state = BudState.DORMANT;
     }
 }
