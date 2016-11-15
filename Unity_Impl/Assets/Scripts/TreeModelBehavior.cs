e3cf62652e4d898c0f26a3b4e93ffe46e65335f8
Antonin Bernardin <antonin.bernardin@gmail.com>
2016-11-15 17:19:00 +0100

    bounding box display

--- Unity_Impl/Assets/Scripts/TreeModelBehavior.cs
+++ Unity_Impl/Assets/Scripts/TreeModelBehavior.cs
@@ -5,7 +5,16 @@ public class TreeModelBehavior : MonoBehaviour
 {
     public TreeModel treeModel;
 
+#if UNITY_EDITOR
+
     void OnDrawGizmos()
+    {
+        drawMarkers();
+        drawSkeleton();
+        drawBoundingBox();
+    }
+
+    void drawMarkers()
     {
         Gizmos.color = Color.yellow;
         foreach (Vector3 marker in treeModel.markers)
@@ -13,4 +22,44 @@ public class TreeModelBehavior : MonoBehaviour
             Gizmos.DrawSphere(transform.position + marker, 0.02f); // TODO : need translation
         }
     }
+
+    void drawBoundingBox()
+    {
+        Gizmos.color = Color.blue;
+        
+        Vector3 pos = transform.position;
+        Vector3 box = pos + treeModel.boundingBox;
+
+        Vector3 front_top_left =        new Vector3(pos.x, box.y, pos.z);
+        Vector3 front_top_right =       new Vector3(box.x, box.y, pos.z);
+        Vector3 front_bottom_left =     new Vector3(pos.x, pos.y, pos.z);
+        Vector3 front_bottom_right =    new Vector3(box.x, pos.y, pos.z);
+
+        Vector3 back_top_left =         new Vector3(pos.x, box.y, box.z);
+        Vector3 back_top_right =        new Vector3(box.x, box.y, box.z);
+        Vector3 back_bottom_left =      new Vector3(pos.x, pos.y, box.z);
+        Vector3 back_bottom_right =     new Vector3(box.x, pos.y, box.z);
+
+        Gizmos.DrawLine(front_top_left, front_top_right);
+        Gizmos.DrawLine(front_top_right, front_bottom_right);
+        Gizmos.DrawLine(front_bottom_right, front_bottom_left);
+        Gizmos.DrawLine(front_bottom_left, front_top_left);
+
+        Gizmos.DrawLine(back_top_left, back_top_right);
+        Gizmos.DrawLine(back_top_right, back_bottom_right);
+        Gizmos.DrawLine(back_bottom_right, back_bottom_left);
+        Gizmos.DrawLine(back_bottom_left, back_top_left);
+
+        Gizmos.DrawLine(front_top_left, back_top_left);
+        Gizmos.DrawLine(front_top_right, back_top_right);
+        Gizmos.DrawLine(front_bottom_left, back_bottom_left);
+        Gizmos.DrawLine(front_bottom_right, back_bottom_right);
+    }
+
+    void drawSkeleton()
+    {
+        Gizmos.color = Color.cyan;
+    }
+
+#endif
 }
